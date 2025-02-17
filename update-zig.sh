c357eecc71b21b21d668eea0c868fc56aa68ffea
Julien Rouzieres <jrouzieres@scaleway.com>
2025-02-17 17:53:26 +0100

    Unknown func

--- update-zig.sh
+++ update-zig.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # update-zig is a script to easily fetch and install new Zig releases
 
-version=0.1
-
 set -o pipefail
 
-releases="https://ziglang.org/download/index.json"
+version=0.1
+
 arch="$(uname -m)"
 os="$(uname -s | tr "[:upper:]" "[:lower:]")"
-latest="$(wget -qO- "${releases}" | grep -v -E '[0-9\.]+-dev' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | uniq | head -n 1)"
 
-msg "https://ziglang.org/download/${latest}/zig-${os}-${arch}-${latest}.tar.xz"
+base="https://ziglang.org/download"
+latest="$(wget -qO- "${base}/index.json" | grep -v -E '[0-9\.]+-dev' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | uniq | head -n 1)"
 
-wget -O latest.tar https://ziglang.org/download/${latest}/zig-${os}-${arch}-${latest}.tar.xz
+wget -O "zig-${os}-${arch}-${latest}.tar.xz" "${base}/${latest}/zig-${os}-${arch}-${latest}.tar.xz"
