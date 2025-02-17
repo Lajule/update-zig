cfc72104de158aef6e7814c2ce3d3955aa93759b
Julien Rouzieres <jrouzieres@scaleway.com>
2025-02-17 17:26:03 +0100

    First commit

--- update-zig.sh
+++ update-zig.sh
@@ -1,31 +1,15 @@
 #!/usr/bin/env bash
-#
 # update-zig is a script to easily fetch and install new Zig releases
-#
-# Home: https://github.com/lajule/update-zig
-#
-# PIPETHIS_AUTHOR lajule
 
-# ignore runtime environment variables
-# shellcheck disable=SC2153
 version=0.1
 
 set -o pipefail
 
-me=$(basename "$0")
-msg() {
-    echo >&2 "${me}": "$*"
-}
+releases="https://ziglang.org/download/index.json"
+arch="$(uname -m)"
+os="$(uname -s | tr "[:upper:]" "[:lower:]")"
+latest="$(wget -qO- "${releases}" | grep -v -E '[0-9\.]+-dev' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | uniq | head -n 1)"
 
-debug() {
-    [ -n "${DEBUG}" ] && msg debug: "$*"
-}
+msg "https://ziglang.org/download/${latest}/zig-${os}-${arch}-${latest}.tar.xz"
 
-log_stdin() {
-    while read -r i; do
-        msg "${i}"
-    done
-}
-
-# defaults
-msg "${me}"
+wget -O latest.tar https://ziglang.org/download/${latest}/zig-${os}-${arch}-${latest}.tar.xz
