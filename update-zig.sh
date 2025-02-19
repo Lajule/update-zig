722ab7ee2f5c278c9107b29398e5200d3eb7b4c9
Julien Rouzieres <jrouzieres@scaleway.com>
2025-02-19 12:05:17 +0100

    Better version

--- update-zig.sh
+++ update-zig.sh
@@ -9,57 +9,41 @@ arch="$(uname -m)"
 os="$(uname -s | tr "[:upper:]" "[:lower:]")"
 base_url="https://ziglang.org/download"
 install_dir="/usr/local"
-latest=""
-archive=""
+zig_dir="${install_dir}/zig"
 
-find_latest() {
-	latest="$(wget -qO- "${base_url}/index.json" | grep -v -E '[0-9\.]+-dev' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | uniq | head -n 1)"
-	if (( $? != 0 )); then
-		printf '%s: could not find latest version\n' "$me" >&2
-		exit 1
-	fi
-}
+if [[ "$1" == "-v" ]]; then
+	printf '%s: version: %s\n' "$me" "${version}"
+	exit 0
+fi
 
-download() {
-	archive="${install_dir}/zig-${os}-${arch}-${latest}.tar.xz"
-	local from
-	from="${base_url}/${latest}/zig-${os}-${arch}-${latest}.tar.xz"
-	if ! wget -O "${archive}" "${from}"; then
-		printf '%s: could not download archive from: %s\n' "$me" "${from}" >&2
-		exit 1
-	fi
-}
+latest="$(wget -qO- "${base_url}/index.json" | grep -v -E '[0-9\.]+-dev' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | uniq | head -n 1)"
+if (( $? != 0 )); then
+	printf '%s: could not find latest version\n' "$me" >&2
+	exit 1
+fi
 
-untar() {
-	if ! tar -C "${install_dir}" -xf "${archive}"; then
-		printf '%s: could not extract archive: %s\n' "$me" "${archive}" >&2
-		exit 1
-	fi
-}
+archive="zig-${os}-${arch}-${latest}.tar.xz"
+archive_path="${install_dir}/${archive}"
+from_url="${base_url}/${latest}/${archive}"
+if ! wget -O "${archive_path}" "${from_url}"; then
+	printf '%s: could not download archive from: %s\n' "$me" "${from_url}" >&2
+	exit 1
+fi
 
-relink() {
-	local zig_dir
-	zig_dir="${install_dir}/zig"
-	local new_zig_dir
-	new_zig_dir="${install_dir}/zig-${os}-${arch}-${latest}"
-	rm "${zig_dir}"
-	ln -s "${new_zig_dir}" "${zig_dir}"
-}
+if ! tar -C "${install_dir}" -xf "${archive_path}"; then
+	printf '%s: could not extract archive: %s\n' "$me" "${archive_path}" >&2
+	exit 1
+fi
 
-case "$1" in
-	-v)
-		printf '%s: version: %s\n' "$me" "${version}"
-		exit 0;;
-esac
-
-find_latest
-download
-untar
-relink
+rm "${zig_dir}"
+if ! ln -s "${archive_path%.tar.xz}" "${zig_dir}"; then
+	printf '%s: could not relink zig directory: %s\n' "$me" "${zig_dir}" >&2
+	exit 1
+fi
 
 printf "\n"
 printf "HINT: Add zig to your PATH enviroment variable\n"
-printf "	export PATH=\${PATH}:%s\n" "${install_dir}/zig"
+printf "	export PATH=\${PATH}:%s\n" "${zig_dir}"
 printf "\n"
 
 exit 0
