1ac7be7937b4d71eb68e88261764e5ebebac0069
Julien Rouzieres <jrouzieres@scaleway.com>
2025-02-19 15:26:46 +0100

    Even better version

--- update-zig.sh
+++ update-zig.sh
@@ -4,17 +4,14 @@
 set -o pipefail
 
 me="${0##*/}"
-version=0.1
-arch="$(uname -m)"
-os="$(uname -s | tr "[:upper:]" "[:lower:]")"
-base_url="https://ziglang.org/download"
-install_dir="/usr/local"
-zig_dir="${install_dir}/zig"
+version='0.1'
 
 if [[ "$1" == "-v" ]]; then
 	printf '%s: version: %s\n' "$me" "${version}"
 	exit 0
 fi
+
+base_url="https://ziglang.org/download"
 
 latest="$(wget -qO- "${base_url}/index.json" | grep -v -E '[0-9\.]+-dev' | grep -E -o '[0-9]\.[0-9]+(\.[0-9]+)?' | uniq | head -n 1)"
 if (( $? != 0 )); then
@@ -22,11 +19,13 @@ if (( $? != 0 )); then
 	exit 1
 fi
 
-archive="zig-${os}-${arch}-${latest}.tar.xz"
+install_dir="/usr/local"
+archive="zig-$(uname -s | tr "[:upper:]" "[:lower:]")-$(uname -m)-${latest}.tar.xz"
 archive_path="${install_dir}/${archive}"
-from_url="${base_url}/${latest}/${archive}"
-if ! wget -O "${archive_path}" "${from_url}"; then
-	printf '%s: could not download archive from: %s\n' "$me" "${from_url}" >&2
+archive_url="${base_url}/${latest}/${archive}"
+
+if ! wget -O "${archive_path}" "${archive_url}"; then
+	printf '%s: could not download archive from: %s\n' "$me" "${archive_url}" >&2
 	exit 1
 fi
 
@@ -35,7 +34,9 @@ if ! tar -C "${install_dir}" -xf "${archive_path}"; then
 	exit 1
 fi
 
+zig_dir="${install_dir}/zig"
 rm "${zig_dir}"
+
 if ! ln -s "${archive_path%.tar.xz}" "${zig_dir}"; then
 	printf '%s: could not relink zig directory: %s\n' "$me" "${zig_dir}" >&2
 	exit 1
